54475a65e70dc9e0d149e77a5898d7bbebdc42f2
unknown <GAder@DUBLT01926.esri-holdings.com>
2017-04-28 12:24:48 +0100

    init commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularPortfolioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularPortfolioApp
+ */
+angular.module('angularPortfolioApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
